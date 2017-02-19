f360cbf3b6c8aa80d36b44e6f1d9d6c39cdaa768
antho <antho@DESKTOP-5N8P24O>
2017-02-19 15:23:11 -0500

    ajout de PetaPoco à la démo

---
+++ MicroOrmDemo.net/PetaPoco/PetaPocoQueries.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MicroOrmDemo.net.PetaPocoSample
+{
+    public class PetaPocoQueries
+    {
+        public List<Orders> GetOrders()
+        {
+            using (var db = new PetaPoco.Database("AdventureWorks2014"))
+            {
+                return db.Query<Orders>(@"SELECT TOP 500 [WorkOrderID], P.Name, [OrderQty], [DueDate] 
+                                 FROM [AdventureWorks2014].[Production].[WorkOrder] AS WO 
+                                 INNER JOIN[Production].[Product] AS P ON P.ProductID = WO.ProductID").ToList();
+            }
+        }
+    }
+}
--- MicroOrmDemo.net/Program.cs
+++ MicroOrmDemo.net/Program.cs
@@ -2,6 +2,7 @@
 using MicroOrmDemo.net.EF;
 using MicroOrmDemo.net.Massive;
 using MicroOrmDemo.net.OrmLite;
+using MicroOrmDemo.net.PetaPocoSample;
 using MicroOrmDemo.net.Simple.Data;
 using System;
 using System.Collections.Generic;
@@ -17,11 +18,11 @@ namespace MicroOrmDemo.net
         static void Main(string[] args)
         {
 
-            Fire1();
-            //Fire2();
+            SingleCall();
+            SingleCall();
         }
 
-        private static void Fire1()
+        private static void SingleCall()
         {
             //Entity Framework
             var efquery = new EfQueries();
@@ -79,68 +80,19 @@ namespace MicroOrmDemo.net
             //watch.Stop();
             //Console.WriteLine("Simple Data strongly typed : " + watch.ElapsedMilliseconds);
 
+            var petaPocoQuery = new PetaPocoQueries();
+            watch = new Stopwatch();
+            watch.Start();
+            var dataPetaPoco = petaPocoQuery.GetOrders();
+            watch.Stop();
+            Console.WriteLine("PetaPoco dynamic: " + watch.ElapsedMilliseconds);
+
             Console.ReadLine();
         }
 
-        private static void Fire2()
+        private static void MultipleIteration(int calls)
         {
-            //Entity Framework
-            var efquery = new EfQueries();
-            var watch = new Stopwatch();
-            watch.Start();
-            var dataEF = efquery.GetOrders();
-            watch.Stop();
-            Console.WriteLine("EF: " + watch.ElapsedMilliseconds);
-
-            //Dapper
-            var dapperquery = new DapperQueries();
-            watch = new Stopwatch();
-            watch.Start();
-            var dataDapper = dapperquery.GetOrders();
-            watch.Stop();
-            Console.WriteLine("Dapper: " + watch.ElapsedMilliseconds);
-
-            //massive dynamic
-            //var massivequery = new MassiveQueries();
-            //watch = new Stopwatch();
-            //watch.Start();
-            //var dataMassiveDynamic = massivequery.GetOrdersDynamic();
-            //watch.Stop();
-            //Console.WriteLine("Massive dynamic: " + watch.ElapsedMilliseconds);
-
-            //massive dynamic to Strongly typed
-            var massivequery = new MassiveQueries();
-            watch = new Stopwatch();
-            watch.Start();
-            var dataMassiveTyped = massivequery.GetOrders();
-            watch.Stop();
-            Console.WriteLine("Massive strongly typed: " + watch.ElapsedMilliseconds);
-
-            //Orm lite
-            var ormLiteQuery = new OrmLiteQueries();
-            watch = new Stopwatch();
-            watch.Start();
-            var dataormLite = ormLiteQuery.GetOrders();
-            watch.Stop();
-            Console.WriteLine("Orm Lite: " + watch.ElapsedMilliseconds);
-
-            //Simple data dynamic 
-            //var simpleDataQuery = new SimpleDataQueries();
-            //watch = new Stopwatch();
-            //watch.Start();
-            //var datasimpleDataDynamic = simpleDataQuery.GetOrdersDynamic();
-            //watch.Stop();
-            //Console.WriteLine("Simple Data dynamic: " + watch.ElapsedMilliseconds);
-
-            //Simple data dynamic to strongly typed
-            var simpleDataQuery = new SimpleDataQueries();
-            watch = new Stopwatch();
-            watch.Start();
-            var datasimpleData = simpleDataQuery.GetOrders();
-            watch.Stop();
-            Console.WriteLine("Simple Data strongly typed : " + watch.ElapsedMilliseconds);
-
-            Console.ReadLine();
+            
         }
     }
 }
